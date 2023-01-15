b3726e8a2c113566dcacc87f280d887eba4299c1
IvanLopez19 <00181217@uca.edu.sv>
2023-01-14 18:23:56 -0600

    I just created a basic server, index.js file

---
+++ Routes/mainroutes.js
@@ -0,0 +1,10 @@
+import express  from "express";
+
+//Creating a separated module for the routes
+function routes(app){
+    
+    //Route to the questions module
+    app.use('/questions', );
+}
+
+export {routes};
